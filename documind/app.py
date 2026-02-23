ba03047ffc983425ec3ce054bce235eecca72a24
Prabhath <prabhathgithub25@gmail.com>
2026-02-23 15:17:36 +0530

    Week3 Day2: Ask API with POST request body created

--- documind/app.py
+++ documind/app.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
+class Question(BaseModel):
+    query: str
+
 @app.get("/")
 def home():
-    return {"message": "DocuMind API is running successfully"}
+    return {"message": "DocuMind API Running"}
+
+@app.post("/ask")
+def ask_question(q: Question):
+    return {
+        "question_received": q.query,
+        "answer": "This is a test response from DocuMind API"
+    }
